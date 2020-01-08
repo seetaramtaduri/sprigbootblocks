b69f73cba60954e5a0e7628d6daa33aff4eae935
Seeta Rama Rao <seetaram.taduri@gmail.com>
2020-01-08 13:10:10 +0530

    First Commit

---
+++ src/main/java/com/stacksimpilfy/webservices/SpringbootbuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimpilfy.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimpilfy/webservices/SpringbootbuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimpilfy.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
